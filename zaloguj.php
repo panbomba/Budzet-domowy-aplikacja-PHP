b7a945010564e5ebf8f67bd00f87e47310fe3bcb
Maciej Bombik <maciej.bombik.programista@gmail.com>
2019-11-21 22:15:21 +0100

    zaloguj.php update

--- zaloguj.php
+++ zaloguj.php
@@ -2,6 +2,12 @@
 
 	session_start();
 	
+	if((!isset($_POST['login'])) || (!isset($_POST['password'])))
+	{
+		header('Location: pierwszy-screen.php');
+		exit();		
+	}
+	
 	require_once "connect.php";
 	$polaczenie = @new mysqli($host, $db_user, $db_password, $db_name);
 	
